c7c66e4947b348bde28d24a28c31a5d8c2aae416
Huynh Duc Dung <dunghd.it@gmail.com>
2024-04-09 21:56:06 +0800

    init project

---
+++ index.ts
@@ -0,0 +1,16 @@
+/**
+ * A module providing a way to run a command on tsx watch
+ *
+ * @link https://github.com/privatenumber/tsx/issues/521
+ * @module
+ */
+
+/**
+ * Run a command on tsx watch
+ *
+ * @param cmd - The command to run
+ */
+export function tsxWatch(cmd: string) {
+	console.log(`Re-run command on tsx watch: ${cmd}`);
+}
+
